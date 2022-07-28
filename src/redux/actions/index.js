052b9b32784a34723def2cb52cacb1b034f0912d
Lucas <lucaschmain@gmail.com>
2022-07-28 16:29:45 -0300

    concluindo requisito 1

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,6 @@
 // Coloque aqui suas actions
+const LOGIN_SUCESS = 'LOGIN_SUCESS';
+const HANDLE_WALLET = 'HANDLE_WALLET';
+
+export const loginAction = (payload) => ({ type: LOGIN_SUCESS, payload });
+export const walletAction = () => ({ type: HANDLE_WALLET });
