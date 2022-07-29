f8ae190348161e69af4b0309b53dd9fff90201fb
Lucas <lucaschmain@gmail.com>
2022-07-29 15:20:54 -0300

    concluindo requisito 4

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -1,24 +1,46 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { fetchDog } from '../redux/actions';
+import { currenciesRequest,
+  expensesAction, fetchCoin, getCurrencies } from '../redux/actions';
 
 class WalletForm extends Component {
   constructor() {
     super();
 
     this.state = {
+      id: -1,
       value: '',
       description: '',
-      coin: '',
-      payment: '',
-      tag: '',
+      currency: 'USD',
+      method: 'Dinheiro',
+      tag: 'Alimentação',
     };
   }
 
   componentDidMount() {
-    const { fetchDogRequest } = this.props;
-    fetchDogRequest();
+    this.fetchCurrencies();
+  }
+
+  fetchCurrencies = async () => {
+    const { fetchCurrencies, fetchCurrencysRequest } = this.props;
+    const URL = 'https://economia.awesomeapi.com.br/json/all';
+    fetchCurrencysRequest();
+    const promisse = await fetch(URL);
+    const data = await promisse.json();
+    const dataFiltered = Object.entries(data)
+      .filter((coin) => coin[0] !== 'USDT')
+      .map((coinName) => coinName[0]);
+    fetchCurrencies(dataFiltered);
+  }
+
+  sumExpenses = () => {
+    const { dataExpenses, totalSumExpenses } = this.props;
+    const totalExpenses = dataExpenses.reduce((acc, curr) => {
+      acc += (parseFloat(curr.value) * parseFloat(curr.exchangeRates[curr.currency].ask));
+      return acc;
+    }, 0);
+    totalSumExpenses(totalExpenses.toFixed(2));
   }
 
   handleInputs = ({ target }) => {
@@ -28,8 +50,17 @@ class WalletForm extends Component {
     });
   }
 
+  handleClick = () => {
+    const { getExpenses } = this.props;
+    this.setState((prev) => ({ id: prev.id + 1 }), () => {
+      getExpenses(this.state);
+      this.setState({ value: '', description: '' });
+    });
+    setTimeout(() => { this.sumExpenses(); }, 100);
+  }
+
   render() {
-    const { value, description, coin, payment, tag } = this.state;
+    const { value, description, currency, method, tag } = this.state;
     const { dataCoin } = this.props;
     return (
       <fieldset>
@@ -58,13 +89,13 @@ class WalletForm extends Component {
           />
         </label>
 
-        <label htmlFor="coin-id">
+        <label htmlFor="currency-id">
           Moeda:
           <select
             data-testid="currency-input"
-            id="coin-id"
-            name="coin"
-            value={ coin }
+            id="currency-id"
+            name="currency"
+            value={ currency }
             onChange={ this.handleInputs }
           >
             {
@@ -79,72 +110,89 @@ class WalletForm extends Component {
             }
           </select>
 
-          <label htmlFor="payment-id">
-            Forma de pagamento:
-            <select
-              data-testid="method-input"
-              id="payment-id"
-              name="payment"
-              value={ payment }
-              onChange={ this.handleInputs }
-            >
-              <option value="Dinheiro">
-                Dinheiro
-              </option>
-              <option value="Cartão de crédito">
-                Cartão de crédito
-              </option>
-              <option value="Cartão de débito">
-                Cartão de débito
-              </option>
-            </select>
-          </label>
-
-          <label htmlFor="tag-id">
-            Tag:
-            <select
-              data-testid="tag-input"
-              id="tag-id"
-              name="tag"
-              value={ tag }
-              onChange={ this.handleInputs }
-            >
-              <option value="Alimentação">
-                Alimentação
-              </option>
-              <option value="Lazer">
-                Lazer
-              </option>
-              <option value="Trabalho">
-                Trabalho
-              </option>
-              <option value="Transporte">
-                Transporte
-              </option>
-              <option value="Saúde">
-                Saúde
-              </option>
-            </select>
-          </label>
         </label>
+        <label htmlFor="method-id">
+          Forma de pagamento:
+          <select
+            data-testid="method-input"
+            id="method-id"
+            name="method"
+            value={ method }
+            onChange={ this.handleInputs }
+          >
+            <option value="Dinheiro">
+              Dinheiro
+            </option>
+            <option value="Cartão de crédito">
+              Cartão de crédito
+            </option>
+            <option value="Cartão de débito">
+              Cartão de débito
+            </option>
+          </select>
+        </label>
+
+        <label htmlFor="tag-id">
+          Tag:
+          <select
+            data-testid="tag-input"
+            id="tag-id"
+            name="tag"
+            value={ tag }
+            onChange={ this.handleInputs }
+          >
+            <option value="Alimentação">
+              Alimentação
+            </option>
+            <option value="Lazer">
+              Lazer
+            </option>
+            <option value="Trabalho">
+              Trabalho
+            </option>
+            <option value="Transporte">
+              Transporte
+            </option>
+            <option value="Saúde">
+              Saúde
+            </option>
+          </select>
+        </label>
+
+        <button
+          type="button"
+          onClick={ this.handleClick }
+        >
+          Adicionar despesa
+        </button>
       </fieldset>
     );
   }
 }
 
 WalletForm.propTypes = {
-  fetchDogRequest: PropTypes.func.isRequired,
+  fetchCurrencies: PropTypes.func.isRequired,
+  fetchCurrencysRequest: PropTypes.func.isRequired,
+  getExpenses: PropTypes.func.isRequired,
+  totalSumExpenses: PropTypes.func.isRequired,
   dataCoin: PropTypes.arrayOf(
     PropTypes.string,
+  ).isRequired,
+  dataExpenses: PropTypes.arrayOf(
+    PropTypes.object,
   ).isRequired,
 };
 
 const mapStateToProps = (store) => ({
   dataCoin: store.wallet.currencies,
+  dataExpenses: store.wallet.expenses,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  fetchDogRequest: () => dispatch(fetchDog()),
+  fetchCurrencies: (payload) => dispatch(getCurrencies(payload)),
+  fetchCurrencysRequest: () => dispatch(currenciesRequest()),
+  getExpenses: (payload) => dispatch(fetchCoin(payload)),
+  totalSumExpenses: (payload) => dispatch(expensesAction(payload)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(WalletForm);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,28 +1,31 @@
 // Coloque aqui suas actions
 const LOGIN_SUCESS = 'LOGIN_SUCESS';
-const COIN_REQUEST = 'COIN_REQUEST';
+const CURRENCIES_REQUEST = 'CURRENCIES_REQUEST';
 const GET_COIN = 'GET_COIN';
 const FAILED_REQUEST = 'FAILED_REQUEST';
+const GET_CURRENCIES = 'GET_CURRENCIES';
+const SUM_EXPENSES = 'SUM_EXPENSES';
 
 export const loginAction = (payload) => ({ type: LOGIN_SUCESS, payload });
 
-const coinRequest = () => ({ type: COIN_REQUEST });
+export const getCurrencies = (payload) => ({ type: GET_CURRENCIES, payload });
+
+export const expensesAction = (payload) => ({ type: SUM_EXPENSES, payload });
+
+export const currenciesRequest = () => ({ type: CURRENCIES_REQUEST });
 
 const getCoin = (payload) => ({ type: GET_COIN, payload });
 
 const failedRequest = (payload) => ({ type: FAILED_REQUEST, payload });
 
-export function fetchDog() {
+export function fetchCoin(payload) {
   return async (dispatch) => {
-    dispatch(coinRequest());
+    dispatch(currenciesRequest());
     try {
       const URL = 'https://economia.awesomeapi.com.br/json/all';
       const promisse = await fetch(URL);
       const data = await promisse.json();
-      const dataFiltered = Object.entries(data)
-        .filter((coin) => coin[0] !== 'USDT')
-        .map((coinName) => coinName[0]);
-      dispatch(getCoin(dataFiltered));
+      dispatch(getCoin([{ ...payload, exchangeRates: { ...data } }]));
     } catch (error) {
       dispatch(failedRequest(error));
     }
