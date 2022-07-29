18e4e46a380a3084fd368df0861d2443a81e3733
Lucas <lucaschmain@gmail.com>
2022-07-28 22:04:19 -0300

    concluindo requisito 3

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,6 +1,30 @@
 // Coloque aqui suas actions
 const LOGIN_SUCESS = 'LOGIN_SUCESS';
-const HANDLE_WALLET = 'HANDLE_WALLET';
+const COIN_REQUEST = 'COIN_REQUEST';
+const GET_COIN = 'GET_COIN';
+const FAILED_REQUEST = 'FAILED_REQUEST';
 
 export const loginAction = (payload) => ({ type: LOGIN_SUCESS, payload });
-export const walletAction = () => ({ type: HANDLE_WALLET });
+
+const coinRequest = () => ({ type: COIN_REQUEST });
+
+const getCoin = (payload) => ({ type: GET_COIN, payload });
+
+const failedRequest = (payload) => ({ type: FAILED_REQUEST, payload });
+
+export function fetchDog() {
+  return async (dispatch) => {
+    dispatch(coinRequest());
+    try {
+      const URL = 'https://economia.awesomeapi.com.br/json/all';
+      const promisse = await fetch(URL);
+      const data = await promisse.json();
+      const dataFiltered = Object.entries(data)
+        .filter((coin) => coin[0] !== 'USDT')
+        .map((coinName) => coinName[0]);
+      dispatch(getCoin(dataFiltered));
+    } catch (error) {
+      dispatch(failedRequest(error));
+    }
+  };
+}
