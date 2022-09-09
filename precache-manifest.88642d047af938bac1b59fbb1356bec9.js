e884004e61c725b86c5e831aea48813a3b8bdd79
Lucas <lucaschmain@gmail.com>
2022-09-09 13:47:50 -0300

    Updates

---
+++ precache-manifest.88642d047af938bac1b59fbb1356bec9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41963d2c18b94f656b4b05192cb3cce7",
+    "url": "/trybewallet/index.html"
+  },
+  {
+    "revision": "8b0396571240483a7c41",
+    "url": "/trybewallet/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "af96eb947bae5486cb71",
+    "url": "/trybewallet/static/js/2.b7511313.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybewallet/static/js/2.b7511313.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b0396571240483a7c41",
+    "url": "/trybewallet/static/js/main.ff73b660.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/trybewallet/static/js/main.ff73b660.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dc72ebdcdec490a7442",
+    "url": "/trybewallet/static/js/runtime-main.ec83dc77.js"
+  }
+]);
