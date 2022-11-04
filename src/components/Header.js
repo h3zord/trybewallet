5a64b1aa35b3e36643be1313627fd2b13ed71e7b
Lucas <lucaschmain@gmail.com>
2022-11-03 22:59:20 -0300

    css Login and header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import styles from './Header.module.css';
+import logoWallet from '../images/logoTrybeWallet.svg';
+import coin from '../images/Vector.svg';
+import avatar from '../images/Vector1.svg';
 
 class Header extends Component {
   render() {
     const { getEmail, getExpenses } = this.props;
     return (
-      <div>
-        <div>
-          <span>Email: </span>
+      <div className={ styles.container }>
+        <img src={ logoWallet } alt="logo wallet trybe" />
+        <div className={ styles.expense }>
+          <img src={ coin } alt="logo coin" />
+          <p>Despesa Total: </p>
+          <span data-testid="total-field">{ getExpenses.toFixed(2) }</span>
+          <span data-testid="header-currency-field"> BRL</span>
+        </div>
+        <div className={ styles.email }>
+          <img src={ avatar } alt="avatar" />
           <span data-testid="email-field">{ getEmail }</span>
         </div>
-        <span>Despesa Total: </span>
-        <span data-testid="total-field">{ getExpenses.toFixed(2) }</span>
-        <span data-testid="header-currency-field"> BRL</span>
       </div>
     );
   }
