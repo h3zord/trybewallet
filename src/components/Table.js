ffa3bee4dd6dc4b6f9acd9b9e2a1250d2a83985f
Lucas <lucaschmain@gmail.com>
2022-11-17 19:18:18 -0300

    aplicando css

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { editExpense, expensesAction, updateExpense } from '../redux/actions';
+import styles from './Table.module.css';
 
 class Table extends Component {
   deleteExpense = ({ target }) => {
@@ -26,62 +27,65 @@ class Table extends Component {
   render() {
     const { dataExpenses } = this.props;
     return (
-      <table>
-        <thead>
-          <tr>
-            <th scope="col">Descrição</th>
-            <th scope="col">Tag</th>
-            <th scope="col">Método de pagamento</th>
-            <th scope="col">Valor</th>
-            <th scope="col">Moeda</th>
-            <th scope="col">Câmbio utilizado</th>
-            <th scope="col">Valor convertido</th>
-            <th scope="col">Moeda de conversão</th>
-            <th scope="col">Editar/Excluir</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            dataExpenses.map((expense) => (
-              <tr key={ expense.id }>
-                <td>{ expense.description }</td>
-                <td>{ expense.tag }</td>
-                <td>{ expense.method }</td>
-                <td>{ parseFloat((expense.value)).toFixed(2) }</td>
-                <td>{ expense.exchangeRates[expense.currency].name }</td>
-                <td>
-                  { parseFloat(expense.exchangeRates[expense.currency].ask).toFixed(2) }
-                </td>
-                <td>
-                  {
-                    (parseFloat(expense.value)
-                    * parseFloat(expense.exchangeRates[expense.currency].ask)).toFixed(2)
-                  }
-                </td>
-                <td>Real</td>
-                <td>
-                  <button
-                    type="button"
-                    data-testid="edit-btn"
-                    className={ expense.id }
-                    onClick={ this.editExpense }
-                  >
-                    Editar
-                  </button>
-                  <button
-                    type="button"
-                    data-testid="delete-btn"
-                    className={ expense.id }
-                    onClick={ this.deleteExpense }
-                  >
-                    Excluir
-                  </button>
-                </td>
-              </tr>
-            ))
-          }
-        </tbody>
-      </table>
+      <div className={ styles.mainContainer }>
+        <table className={ styles.container }>
+          <thead>
+            <tr className={ styles.teste }>
+              <th scope="col">Descrição</th>
+              <th scope="col">Tag</th>
+              <th scope="col">Método de pagamento</th>
+              <th scope="col">Valor</th>
+              <th scope="col">Moeda</th>
+              <th scope="col">Câmbio utilizado</th>
+              <th scope="col">Valor convertido</th>
+              <th scope="col">Moeda de conversão</th>
+              <th scope="col">Editar/Excluir</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              dataExpenses.map((expense) => (
+                <tr key={ expense.id }>
+                  <td>{ expense.description }</td>
+                  <td>{ expense.tag }</td>
+                  <td>{ expense.method }</td>
+                  <td>{ parseFloat((expense.value)).toFixed(2) }</td>
+                  <td>{ expense.exchangeRates[expense.currency].name }</td>
+                  <td>
+                    { parseFloat(expense.exchangeRates[expense.currency].ask).toFixed(2) }
+                  </td>
+                  <td>
+                    {
+                      (parseFloat(expense.value)
+                      * parseFloat(expense.exchangeRates[expense.currency].ask))
+                        .toFixed(2)
+                    }
+                  </td>
+                  <td>Real</td>
+                  <td>
+                    <button
+                      type="button"
+                      data-testid="edit-btn"
+                      className={ expense.id }
+                      onClick={ this.editExpense }
+                    >
+                      Editar
+                    </button>
+                    <button
+                      type="button"
+                      data-testid="delete-btn"
+                      className={ expense.id }
+                      onClick={ this.deleteExpense }
+                    >
+                      Excluir
+                    </button>
+                  </td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
